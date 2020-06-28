69f92502c358b86e8ab248e052dee0f491dcccd0
Dilan Madusanka <madmadusanak@gmail.com>
2020-06-28 17:48:00 +0530

    initial commit

---
+++ LoginApp/LoginApp/ViewModel/LoginViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LoginApp.ViewModel 
+{
+    class LoginViewModel : INotifyPropertyChanged
+    {
+        private string password;
+        private string username;
+        private Dictionary<string, string> userData = new Dictionary<string, string>(){ 
+            {"dilan","123"} };
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public LoginViewModel() {
+            LoginCommand = new Command(Authenticate); ;
+        }
+        public string Username
+        {
+           
+            set {
+                username = value;
+
+            }
+        }
+        public string Password
+        {
+            set { password = value; }
+        }
+        public Command LoginCommand
+        {
+            get;
+
+    }
+        void Authenticate() {
+            if(username==null || password == null)
+            {
+                Application.Current.MainPage.DisplayAlert("Login State", "Wrong credentials", "OK");
+            }
+            else if (userData.ContainsKey(username))
+            {
+                if (userData[username]==password) {
+                    Application.Current.MainPage.DisplayAlert("Login State","Succesfull Login", "OK");
+                }
+               else Application.Current.MainPage.DisplayAlert("Save", "Wrong credentials", "OK");
+            }
+             else  Application.Current.MainPage.DisplayAlert("Login State", "Wrong credentials", "OK"); 
+   
+
+
+        }            
+    }
+   
+}
